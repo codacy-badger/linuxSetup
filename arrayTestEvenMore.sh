0c5311bce3b17feff62716998119ac3f4744d987
xmetal <Xmetalfanx@yahoo.com>
2020-06-12 14:29:54 -0400

    Array testing that works

--- arrayTestEvenMore.sh
+++ arrayTestEvenMore.sh
@@ -5,7 +5,7 @@
 # just an idea to put EOL info in here
 
 # https://en.wikipedia.org/wiki/Fedora_version_history
-currentFedora=(
+declare -A currentFedora=(
     [31]="Not known"
     [32]="Not known"
     [rawhide]="Rolling"
@@ -13,14 +13,13 @@ currentFedora=(
 
 # how do i handle this .. 15.1?   151? for version 15.1
 # https://en.opensuse.org/Lifetime
-currentOpenSuse=(
-    [151]="November 2020"
+declare -A currentOpenSUSE=(
+    [15.1]="November 2020"
     [tumbleweed]="Rolling"
 )
 
 ## Idea: use "End of Standard Support" and not "End of Life" listed on https://wiki.ubuntu.com/Releases
-declare -A currentUbuntu
-currentUbuntu=(
+declare -A currentUbuntu=(
     [xenial]="April 2021"         # 16.04
     [bionic]="April 2023"         # 18.04
     [focal]="April 2025"          # 20.04
@@ -29,15 +28,26 @@ currentUbuntu=(
 
 #lsbCodeName="$(lsb_release -sc)"
 
-lsbCodeName="bionic"
+## OpenSuse Testing atm
+
+OSRELEASE_VERSION_ID="15.0"
 #####################################
 
+clear
+
+echo -e "currentOpenSUSE[$OSRELEASE_VERSION_ID]"
+echo
+
+
 #if relealsbCodeNameseIAmOn is in the array
-if [ -v "currentUbuntu[$lsbCodeName]" ]; then
-    echo "You are on a current release of Ubuntu"
+if [ -v "currentOpenSUSE[$OSRELEASE_VERSION_ID]" ]; then
+
+	echo -e "${currentOpenSUSE[$OSRELEASE_VERSION_ID]}"
+	echo
+    echo "You are on a current release of openSuse"
     echo
-    echo -e "${currentUbuntu[$lsbCodeName]}"
+    echo -e "The end of life/support for your release is: ${currentOpenSUSE[$OSRELEASE_VERSION_ID]}"
 
 else
-    echo "You are NOT on a current release of Ubuntu"
+    echo "You are NOT on a current release of openSUSE"
 fi
